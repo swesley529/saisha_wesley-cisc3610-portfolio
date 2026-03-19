726de19d7d1e5c14d36c9bb51957f2d12a94a9fd
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-19 17:36:39 -0400

    Update mycartoon.js

--- pages/mycartoon.js
+++ pages/mycartoon.js
@@ -20,6 +20,9 @@ window.onload = function () {
 //ground
   ctx.fillStyle = "rgb(101, 67, 33)";
   ctx.fillRect(0, canvas.height - 200, canvas.width, 200);
-  ctx.stroke();
+
+  //house
+  ctx.fillStyle = "rbg(255, 0, 0)";
+  ctx.fillRect(150, 300, 200, 200);
 
 };

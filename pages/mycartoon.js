096bb0b1db22b10eec5f3beb31e9ebe2dd159a65
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-19 16:40:28 -0400

    Update mycartoon.js

--- pages/mycartoon.js
+++ pages/mycartoon.js
@@ -1,1 +1,18 @@
 //my javascript code for my website
+const canvs = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+
+//background 
+ctx.background = new THREE.Color("skyblue");
+
+//sun
+ctx.beginPath();
+ctx.arc(112, 64, 50, 0, 2 * Math.PI);
+ctx.stroke();
+
+//ground
+ctx.fillStyle = "rbg(101, 67, 33)";
+ctx.fillRect(0, 270, 800, 200);
+ctx.stroke();
+
+

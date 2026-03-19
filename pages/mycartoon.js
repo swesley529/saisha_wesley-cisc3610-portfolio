d633465395bc5c1bd1d5bc5c6f2560c3e54a1646
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-19 17:21:49 -0400

    Update mycartoon.js

--- pages/mycartoon.js
+++ pages/mycartoon.js
@@ -1,20 +1,25 @@
 //my javascript code for my website
 window.onload = function () {
-const canvas = document.getElementById("myCanvas");
-const ctx = canvas.getContext("2d");
+  const canvas = document.getElementById("myCanvas");
+  const ctx = canvas.getContext("2d");
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
 //background 
-ctx.fillStyle = "skyblue";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.fillStyle = "skyblue";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 //sun
-ctx.beginPath();
-ctx.arc(112, 64, 50, 0, 2 * Math.PI);
-ctx.stroke();
+  ctx.beginPath();
+  ctx.arc(112, 64, 50, 0, 2 * Math.PI);
+  ctx.fillStyle = "yellow";
+  ctx.strokeStyle = "yellow";
+  ctx.fill();
+  ctx.stroke();
 
 //ground
-ctx.fillStyle = "rgb(101, 67, 33)";
-ctx.fillRect(0, 270, 800, 200);
-ctx.stroke();
+  ctx.fillStyle = "rgb(101, 67, 33)";
+  ctx.fillRect(0, 270, 800, 200);
+  ctx.stroke();
 
 };

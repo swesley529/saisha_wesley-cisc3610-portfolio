9eeac5b968de9f008448aedf01378368ec98a85a
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-03-24 15:15:16 -0400

    Update mycartoon.js

--- pages/mycartoon.js
+++ pages/mycartoon.js
@@ -1,12 +1,15 @@
 //my javascript code for my website
 window.onload = function () {
   const canvas = document.getElementById("myCanvas");
-  const ctx = canvas.getContext("2d");
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
 
-//background 
-  ctx.fillStyle = "skyblue";
+canvas.style.border = "3px solid black";
+
+canvas.width = "800";
+canvas.height = "800";
+
+const ctx = canvas.getContext("2d");
+
+ ctx.fillStyle = "skyblue";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 //sun
@@ -24,6 +27,39 @@ window.onload = function () {
   //house
   ctx.fillStyle = "rgb(255, 0, 0)";
   ctx.fillRect(canvas.width/2 - 200, canvas.height - 500, 400, 300);
+
+//roof
+ctx.beginPath();
+ctx.strokeStyle = "black";
+ctx.moveTo(400, 200);
+ctx.lineTo(180, 300);
+ctx.lineTo(620, 300);
+ctx.closePath();
+ctx.fillStyle = "black";
+ctx.fill();
+ctx.stroke();
+
+//windows
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.fillRect(250, 350, 70, 70);
+
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.fillRect(480, 350, 70, 70);
+
+
+//door
+ctx.beginPath();
+ctx.fillStyle = "pink";
+ctx.fillRect(370, 470, 70, 130);
+
+//knob
+ctx.beginPath();
+ctx.fillStyle = "black";
+ctx.arc(422, 540, 7, 0, 2 * Math.PI);
+ctx.fill();
+ctx.stroke();
 
 
 };

a09b3f5a44c92a1d24172c64557fa4a601d64204
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-04-23 16:22:32 -0400

    Update wordsproject.js

--- pages/wordsproject.js
+++ pages/wordsproject.js
@@ -1,6 +1,6 @@
 function syncSentence() {
-const menu = document.getElementById("wordSelect");
-const display = document.getElementById("sentenceDisplay");
+const menu = document.getElementById("wordDisplay").textContent = selectedWord;
+const display = document.getElementById("sentenceDisplay").textContent = selectedSentence;
 if (menu.selectedIndex > 0) {
 const selectedOption = menu.options[menu.selectedIndex];
 // Extract and Display

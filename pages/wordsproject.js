76a6e6f8f8d44005d7eafdcf8430975a0c229e58
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-04-23 16:06:00 -0400

    Update wordsproject.js

--- pages/wordsproject.js
+++ pages/wordsproject.js
@@ -1,1 +1,11 @@
-
+function syncSentence() {
+const menu = document.getElementById("wordSelect");
+const display = document.getElementById("sentenceDisplay");
+if (menu.selectedIndex > 0) {
+const selectedOption = menu.options[menu.selectedIndex];
+// Extract and Display
+display.value = selectedOption.dataset.temp;
+} else {
+display.value = "";
+}
+}

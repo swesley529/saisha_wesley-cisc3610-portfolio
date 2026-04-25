2bafbc9348e7b268d51191f4ac8199904d0ccd8c
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-04-24 23:27:01 -0400

    Update wordsproject.js

--- pages/wordsproject.js
+++ pages/wordsproject.js
@@ -1,11 +1,15 @@
 function syncSentence() {
-const menu = document.getElementById("wordDisplay").textContent = selectedWord;
-const display = document.getElementById("sentenceDisplay").textContent = selectedSentence;
+  const menu = document.getElementById("wordSelect");
+  const wordDisplay = document.getElementById("wordDisplay");
+  const sentenceDisplay = document.getElementById("sentenceDisplay");
+  
 if (menu.selectedIndex > 0) {
-const selectedOption = menu.options[menu.selectedIndex];
-// Extract and Display
-display.value = selectedOption.dataset.temp;
+  const selectedOption = menu.options[menu.selectedIndex];
+  wordDisplay.textContent = selectedOption.value;
+  sentenceDisplay.textContent = selectedOption.dataset.sentence;
+  speakWord();
 } else {
-display.value = "";
-}
+  wordDisplay.textContent = "You didn't select a word. Please select a word.";
+  sentenceDisplay.textContent = "You didn't select a sentence. Please select a sentence."
+  }
 }

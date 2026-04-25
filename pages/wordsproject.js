0615cb402022f4853be591ec54eeab08925c2acb
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-04-24 23:34:24 -0400

    Update wordsproject.js

--- pages/wordsproject.js
+++ pages/wordsproject.js
@@ -1,3 +1,17 @@
+function speak(textToSay) {
+  const message = new SpeechSynthesisUtterance(textToSay);
+  message.pitch = 1.2;
+  message.rate = 1.0;
+  window.speechSynthesis.speak(message);
+}
+function speakWord() {
+  const words = document.getElementById("wordDisplay");
+  speak(words);
+}
+function speakSentence() {
+  const words = document.getElementById("sentenceDisplay");
+  speak(words);
+}
 function syncSentence() {
   const menu = document.getElementById("wordSelect");
   const wordDisplay = document.getElementById("wordDisplay");

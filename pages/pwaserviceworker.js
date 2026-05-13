cd30ee5d1099e5f385fce7c3e698986e13f6d6c6
swesley529 <159355117+swesley529@users.noreply.github.com>
2026-05-13 15:56:46 -0400

    Update pwaserviceworker.js

--- pages/pwaserviceworker.js
+++ pages/pwaserviceworker.js
@@ -1,1 +1,53 @@
+const CACHE_NAME = 'wellnessguide-v1';
 
+const FILES_TO_CACHE = [
+  'pwaproject.html',
+  'pwaapp.js',
+  'pwastyle.css',
+  'pwadata.json',
+  'pwamanifest.json',
+  'icon-192.png',
+  'icon-512.png',
+  'meditation.png',
+  'journaling.png',
+  'exercise.png',
+  'thirdspaces.png',
+  'hobbies.png',
+  'audio/meditation.mp3',
+  'audio/journaling.mp3',
+  'audio/exercise.mp3',
+  'audio/thirdspaces.mp3',
+  'audio/hobbies.mp3'
+];
+
+// install
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('Caching files');
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+// activate
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+//when offline
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
